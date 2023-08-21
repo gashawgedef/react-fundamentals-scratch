33c4bd43e8e0e43ae967699f5a5cba653939fbe6
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-08-21 13:42:26 -0700

    gashity

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Great from './components/Great';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <Great/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
