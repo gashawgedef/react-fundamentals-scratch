8bb36df7bb459ac3cb6331afcb62889c22b9a593
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-08-21 11:49:16 -0700

    Gashaw Gedef

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Great from './components/Great';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Great/>
+      <Welcome/>
     </div>
   );
 }
--- helloworld/src/components/Great.js
+++ helloworld/src/components/Great.js
@@ -1,5 +1,7 @@
 import React from 'react'
-function  Great(){
-    return <h1>Gashaw Gedef</h1>
-}
+// function  Great(){
+//     return <h1>Gashaw Gedef</h1>
+// }
+
+const Great=()=><h1>Hello Gashaw Gedef</h1>
 export default Great
