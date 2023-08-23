7d995377084cb7985afcfabf03605f713102ab46
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-08-23 11:18:51 -0700

    gashaw

---
+++ helloworld/src/components/Toolbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+function AlertButton({ message, children }) {
+    return (
+      <button onClick={() => alert(message)}>
+        {children}
+      </button>
+    );
+  }
+
+  export default function Toolbar() {
+    return (
+      <div>
+        <AlertButton message="Playing!">
+          Play Movie
+        </AlertButton>
+        <AlertButton message="Uploading!">
+          Upload Image
+        </AlertButton>
+      </div>
+    );
+  }
