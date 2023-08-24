4588ba3133338874865d4c48474330ec6eb94fa2
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-08-24 08:40:54 -0700

    gedef shibabaw

---
+++ helloworld/src/components/Gashaw.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Gashaw extends Component {
+  render() {
+    return (
+      <div>
+        Gashity
+      </div>
+    )
+  }
+}
+
+export default Gashaw
